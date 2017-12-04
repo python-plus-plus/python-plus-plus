b35e84716132ff045c864005fc98bf58f74c08dd
William Qi <hello@williamqi.com>
2017-12-03 23:34:48 -0800

    v0.1.1 update pypi config

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
 setup(
     name='python-plus-plus',
-    packages=['ppp_lib'],  # this must be the same as the name above
+    packages=['ppp_lib'],
     entry_points={
         "console_scripts": ['ppp = ppp_lib.__main__:main']
     },
-    version='0.1',
+    version='0.1.1',
     description='All the functionality of Python, with less of the annoyances.',
     author='Jason Chan, James Liu, Shikib Mehri, William Qi',
-    author_email='',
-    url='https://github.com/python-plus-plus/python-plus-plus',  # use the URL to the github repo
-    download_url='https://github.com/python-plus-plus/python-plus-plus',  # I'll explain this in a second
-    keywords=['python++', 'tail call optimization', 'mutable default', 'deep copy', '++'],  # arbitrary keywords
+    url='https://github.com/python-plus-plus/python-plus-plus',
+    download_url='https://github.com/python-plus-plus/python-plus-plus/archive/0.1.1.tar.gz',
+    keywords=['python++', 'tail call optimization', 'mutable default', 'deep copy', '++'],
     classifiers=[],
 )
